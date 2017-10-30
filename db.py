e63dd0445237958ef99c07f30b073fbe8a8d307a
n2omatt <n2omatt@amazingcow.com>
2017-10-29 23:08:56 -0200

    - Reimplemented the db things.

--- db.py
+++ db.py
@@ -3,25 +3,90 @@
 ################################################################################
 ## Python
 import sqlite3;
+import pdb;
+## instacrawler
+from config import *;
+from log    import *;
 
 
+################################################################################
+## Private Vars                                                               ##
+################################################################################
+_db_created = False;
+
+
+################################################################################
+## Public Functions                                                           ##
+################################################################################
+def db_exists_url(url):
+    if(config.ignore_db):
+        return False;
+
+    log.D("[DB] Check if url exists\n\tURL: {0}", url);
+    rows = _query("SELECT * FROM MediaPageUrls WHERE url = ?", url);
+
+    return len(rows) != 0;
+
+def db_insert_media_url(url):
+    if(config.ignore_db):
+        return;
+
+    log.D("[DB] Inserting media.\n\tURL: {0}", url);
+    _execute("INSERT INTO MediaPageUrls (url) VALUES (?)", url);
+
+
+################################################################################
+## Private Functions                                                          ##
+################################################################################
 def _open_db():
-    con = sqlite3.connect("DB2.db");
-    try:
-        cur = con.cursor();
-        cur.execute("CREATE TABLE MediaPageUrls (id INTEGER PRIMARY KEY, url TEXT)");
-        con.commit();
-    except:
-        pass;
+    if(config.ignore_db):
+        return;
+
+    con = sqlite3.connect(config.db_name);
+    if(not _db_created):
+        _create_db(con);
 
     return con;
 
+def _create_db(con):
+    global _db_created;
 
-def db_exists_url(url):
-    return False;
+    if(_db_created):
+        return;
 
-def db_insert_media_url(url):
-    return None;
+    _db_created = True;
+    try:
+        log.D("[DB] Creating DB....");
+        _execute("CREATE TABLE MediaPageUrls (id INTEGER PRIMARY KEY, url TEXT)");
 
-def db_insert_url(url):
-    return None;
+    except:
+        log.D("[DB] DB already exists - Skipping....");
+
+
+def _query(stmt, *args):
+    con = _open_db();
+    with con:
+        cur = con.cursor();
+        cur.execute(stmt, args);
+        rows = cur.fetchall();
+
+        return rows;
+
+def _execute(stmt, *args):
+    con = _open_db();
+    with con:
+        cur = con.cursor();
+        cur.execute(stmt, args);
+        con.commit();
+
+
+################################################################################
+## "Test"                                                                     ##
+################################################################################
+if __name__ == '__main__':
+    val = db_exists_url("my url");
+    print(val);
+
+    db_insert_media_url("my url");
+    val = db_exists_url("my url");
+    print(val);
